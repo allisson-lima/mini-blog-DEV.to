15bc844aa16e4169252587ae26b6989559db41c4
allisson-lima <allisson.lima.dev@gmail.com>
2025-06-12 14:36:35 -0300

    chore(lib): add config eslint e config do husky pre-commit e push

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,47 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", 'plugin:prettier/recommended'),
+  {
+    ignores: [
+      '**/node_modules/*',
+      '**/out/*',
+      '**/.next/*',
+      '**/coverage',
+      'src/styles/globals.css',
+      '.next',
+      '.cache',
+      'package-lock.json',
+      'yarn.lock',
+      'pnpm-lock.yaml',
+      'public',
+      'next.config.js',
+      'next.config.ts',
+      'next-env.d.ts',
+      'tsconfig.json',
+    ]
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        2,
+        {
+          argsIgnorePattern: '^_'
+        }
+      ],
+
+      'no-console': [
+        2,
+        {
+          allow: ['warn', 'error']
+        }
+      ]
+    }
+  },
 ];
 
 export default eslintConfig;
