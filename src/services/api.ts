91f60404c4a9115fb05330a06556d19ecd3a8067
allisson-lima <allisson.lima.dev@gmail.com>
2025-06-12 09:11:15 -0300

    feat(api): configura cliente HTTP com axios e interceptors

---
+++ src/services/api.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import axios, { AxiosError } from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    console.error("API Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export { api };
