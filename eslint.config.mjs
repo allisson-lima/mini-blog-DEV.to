bfb2eb47c5bfc05540a877dd28c2220df72c0b4e
allisson-lima <allisson.lima.dev@gmail.com>
2025-06-13 15:29:01 -0300

    refactor(eslint): add config rules @typescript-eslint/no-unused-vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,6 +40,8 @@ const eslintConfig = [
   },
   {
     rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars-experimental": "off",
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
       "@typescript-eslint/no-unused-vars": [
