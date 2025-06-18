64058f2ddabf857872efd0288f7d82849acf152d
allisson-lima <allisson.lima.dev@gmail.com>
2025-06-18 10:55:44 -0300

    fix(layout): implementa altura minima no layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <div className="flex-1 container py-6 md:px-0 px-3 mx-auto">
+      <div className="flex-1 container min-h-screen py-6 md:px-0 px-3 mx-auto">
         {children}
       </div>
       <Footer />
